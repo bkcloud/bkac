6788c49c4f798b827a1fe9de017e7907a2daafce
neittien0110 <neittien0110@gmail.com>
2014-07-16 16:45:13 +0700

    Add: Bổ sung script môi trường toàn cục cho mọi script Add: Bổ sung script đầu tiên, để thêm rule vào firewall

---
+++ fw_scripts/env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+firewall_ip=bkac.bkcloud.org	# Địa chỉ IP của tường lừa từ xa
+firewall_ssh_port=22		# Cổng dịch vụ ssh của tường lửa từ xa
+firewall_user=mgnt_center       # Tài khoản sử dụng để đăng nhập tường lửa
